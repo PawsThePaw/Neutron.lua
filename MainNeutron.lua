31325e805892eec8d9f3f6215e05b0b7b0f99c95
Paws <124329589+PawsThePaw@users.noreply.github.com>
2024-06-26 04:23:47 +0800

    Update MainNeutron.lua

--- MainNeutron.lua
+++ MainNeutron.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Neutron.lua/main/NeutronSetup.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Neutron.lua/main/MainSetUp_V1.0.1.txt", true))()
